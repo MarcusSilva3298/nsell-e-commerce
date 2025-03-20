2bfeb1dad53627bdd19f6a1d83d37cab761bfd54
marcussilva <marcussilva3298@gmail.com>
2025-03-20 00:53:59 -0300

    fx: SourceModule name

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SourceModule } from './source.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(SourceModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/source.module.ts
+++ src/source.module.ts
@@ -1,16 +1,7 @@
 import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { validateEnvVariables } from './Infra/Config/configModule/validate';
 import { HttpModule } from './Presenters/Http/http.module';
 
 @Module({
-  imports: [
-    HttpModule,
-    // ConfigModule.forRoot({
-    //   envFilePath: '.env',
-    //   isGlobal: true,
-    //   validate: validateEnvVariables,
-    // }),
-  ],
+  imports: [HttpModule],
 })
-export class SoruceModule {}
+export class SourceModule {}
