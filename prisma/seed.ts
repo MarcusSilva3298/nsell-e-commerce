90de100a44b99cb1f16864a25f465796c17c90eb
marcussilva <marcussilva3298@gmail.com>
2025-03-20 17:15:09 -0300

    fx: admin password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,7 +12,7 @@ async function seed() {
       id: '0195b3ed-727a-72a3-8815-5605193b418d',
       email: adminEmail,
       name: 'admin',
-      password: '$2y$10$u7HmjU8e62TIxhe70./tYeRIBt2f10inhNDM5PacsBF4uruhJuRFq',
+      password: '$2b$06$5oUqtLcaN6Sx/B/PMAvFmuudTowllwkE.gAuFyFMCitbuPMQ4rWZe',
       type: UserType.ADMIN,
       verifiedEmail: true,
     },
