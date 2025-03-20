58167b6090731267d6c987cf9ca6a9bd4cf227b9
marcussilva <marcussilva3298@gmail.com>
2025-03-20 17:15:22 -0300

    ft: use of global pipes and filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from './Presenters/Http/Filters/http-exception.filter';
 import { SourceModule } from './source.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SourceModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
