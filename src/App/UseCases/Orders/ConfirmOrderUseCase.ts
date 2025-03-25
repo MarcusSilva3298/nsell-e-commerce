8465822ec56ecf6c13cb010a64f3a1de4dd981d9
marcussilva <marcussilva3298@gmail.com>
2025-03-25 20:56:39 -0300

    fx: confirm order use case interface

--- src/App/UseCases/Orders/ConfirmOrderUseCase.ts
+++ src/App/UseCases/Orders/ConfirmOrderUseCase.ts
@@ -1,23 +1,31 @@
 import { User } from '@prisma/client';
+import { Order } from '../../../Domain/Entities/Order';
 import { OrderAlreadyPaidException } from '../../../Domain/Errors/Orders/OrderAlreadyPaidException';
 import { OrderNotFoundException } from '../../../Domain/Errors/Orders/OrderNotFoundException';
 import { UnsuccesfulPaymentException } from '../../../Domain/Errors/Orders/UnsuccessfulPaymentException';
 import { NotEnoughInStock } from '../../../Domain/Errors/Products/NotEnoughInStockException';
 import { ConfirmOrderDto } from '../../../Domain/Shared/Dtos/Orders/ConfirmOrderDto';
 import { OrderStatusValuesEnum } from '../../../Domain/Shared/Enums/OrderStatusValuesEnum';
+import { IUseCase } from '../../../Domain/Shared/Interfaces/IUseCase';
 import { UserUtils } from '../../../Domain/Shared/Utils/UserUtils';
 import { IOrdersRepository } from '../../Ports/Repositories/IOrdersRepository';
 import { IProductRepository } from '../../Ports/Repositories/IProductsRepository';
 import { IPaymentService } from '../../Ports/Services/IPaymentService';
 
-export class ConfirmOrderUseCase {
+export class ConfirmOrderUseCase
+  implements IUseCase<Order, [string, User, ConfirmOrderDto]>
+{
   constructor(
     private readonly ordersRepository: IOrdersRepository,
     private readonly productsRepository: IProductRepository,
     private readonly paymentService: IPaymentService,
   ) {}
 
-  async execute(orderId: string, user: User, body: ConfirmOrderDto) {
+  async execute(
+    orderId: string,
+    user: User,
+    body: ConfirmOrderDto,
+  ): Promise<Order> {
     const orderExists = await this.ordersRepository.findById(orderId);
 
     if (!orderExists || UserUtils.isNotOwnerOrAdmin(orderExists.clientId, user))
