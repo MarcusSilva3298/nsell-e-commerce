81bf634239034895be24cc670fb325e929a15552
marcussilva <marcussilva3298@gmail.com>
2025-03-24 22:26:33 -0300

    fx: add status methods to interface

--- src/App/Ports/Repositories/IOrdersRepository.ts
+++ src/App/Ports/Repositories/IOrdersRepository.ts
@@ -1,8 +1,12 @@
 import { Order } from '../../../Domain/Entities/Order';
+import { OrderStatus } from '../../../Domain/Entities/OrderStatus';
 import { OrderItemPersistanceDto } from '../../../Domain/Shared/Dtos/Orders/OrderItemPersistanceDto';
 import { SearchOrdersQueryDto } from '../../../Domain/Shared/Dtos/Orders/SearchOrdersQueryDto';
+import { OrderStatusValuesEnum } from '../../../Domain/Shared/Enums/OrderStatusValuesEnum';
 
 export interface IOrdersRepository {
+  findStatus(value: OrderStatusValuesEnum): OrderStatus;
+  listStatus(): OrderStatus[];
   create(clientId: string, orderItem?: OrderItemPersistanceDto): Promise<Order>;
   findById(id: string): Promise<Order | null>;
   delete(id: string): Promise<Order>;
